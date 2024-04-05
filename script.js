a1ec5afcc23ed817b7f5baa00f0d0558c2eab030
jaypalkadam <jaypalkadam007@gmail.com>
2024-04-05 19:48:18 +0530

    new script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+//ssh-keygen
 
 function addItem(e) {
   e.preventDefault();
